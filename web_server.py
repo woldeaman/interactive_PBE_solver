ac3da25af462601e34b74dd6fcc52644a2ea840d
Amanuel Wolde-Kidan <woldeaman@hiraiwamac.physik.fu-berlin.de>
2019-01-28 20:01:24 +0100

    now plotting both potential and ions

--- web_server.py
+++ web_server.py
@@ -1,11 +1,18 @@
 """Running the server for interactive PBE-solving."""
 import numpy as np
 import pbe_solver.pbe_solver as pbe  # this needs to be installed beforehand
+import scipy.constants as sc
 from bokeh.io import curdoc
-from bokeh.layouts import row, widgetbox
+from bokeh.layouts import widgetbox, gridplot
 from bokeh.models import ColumnDataSource
 from bokeh.models.widgets import Slider
 from bokeh.plotting import figure
+
+##################
+#   ENVIRONMENT  #
+##########################################################################
+nm_3_to_mols = 1E24/sc.Avogadro  # from nm^3 to mol/l
+##########################################################################
 
 
 ###########################
@@ -13,8 +20,9 @@ from bokeh.plotting import figure
 ##########################################################################
 def solver(bins, temp, distance, valency, sigma, c_0_pre):
     """Run the PBE solver."""
-    # TODO: for now without profiles
+    # TODO: implement reading of PMFs, epsilon and charge density profiles
     rho, eps, pmf_cat, pmf_an = np.zeros(bins), np.ones(bins)*(1/80), np.zeros(bins), np.zeros(bins)
+
     # convert units
     (zz_hat, kappa, c_0, beta, dz_hat, sigma_hat, rho_hat) = pbe.convert_units(
         bins, temp, distance, valency, sigma, rho, c_0_pre)
@@ -47,47 +55,48 @@ def update_data(attrname, old, new):
     c = c_0.value
 
     # solve pbe
-    data = solver(b, t, d, z, s, c)
+    pbe_data = solver(b, t, d, z, s, c)
 
     # rewrite data
-    psi_plt.data_source.data = dict(x=data['zz'], y=data['psi'])
-    cat_plt.data_source.data = dict(x=data['zz'], y=data['dens_n'])
-    ani_plt.data_source.data = dict(x=data['zz'], y=data['dens_p'])
+    total_data.data = dict(x=pbe_data['zz']-pbe_data['zz'].max()/2, y0=pbe_data['psi'],
+                           y1=pbe_data['dens_p']*nm_3_to_mols, y2=pbe_data['dens_n']*nm_3_to_mols)
 ##########################################################################
 
 
 ####################
 #      MAIN        #
 ##########################################################################
-# Set up data
-x, y = np.zeros(50), np.zeros(50)
-psi = ColumnDataSource(data=dict(x=x, y=y))
-dens_neg = ColumnDataSource(data=dict(x=x, y=y))
-dens_pos = ColumnDataSource(data=dict(x=x, y=y))
-
-# set up plots
-potential = figure(plot_height=500, plot_width=500, title="electrostatic potential")
-psi_plt = potential.line('x', 'y', source=psi, line_width=3, line_alpha=0.6)
-dens = figure(plot_height=500, plot_width=500, title="ion density")
-cat_plt = dens.line('x', 'y', source=dens_pos, line_width=3, line_alpha=0.6, legend='cation density')
-ani_plt = dens.line('x', 'y', source=dens_neg, line_width=3, line_alpha=0.6, legend='anion density')
-
-
-# Set up widgets
+# set up and initiallize widgets
 bins = Slider(title="bins", value=100, start=50, end=250, step=10)
 temp = Slider(title="temperatur [K]", value=300, start=0, end=500, step=10)
 dist = Slider(title="distance [nm]", value=1.0, start=0.1, end=10, step=0.1)
-valency = Slider(title="valency", value=1, start=1, end=3, step=1)
-sigma = Slider(title="surface charge [e/nm^2]", value=0.1, start=0.5, end=5, step=0.1)
-c_0 = Slider(title="ion concentration [mol/l]", value=0.5, start=1, end=5, step=0.1)
-
+valency = Slider(title="valency", value=1, start=1, end=4, step=1)
+sigma = Slider(title="surface charge [e/nm\u00b2]", value=0, start=-5, end=5, step=0.1)
+c_0 = Slider(title="ion concentration [mol/l]", value=0, start=1, end=5, step=0.1)
 # connect same callback function to all sliders
 for w in [bins, temp, dist, valency, sigma, c_0]:
     w.on_change('value', update_data)
 
+# set up and initiallize data
+zz = np.linspace(-dist.value/2, dist.value/2, bins.value)
+psi_dat, cat_dat, an_dat = np.zeros(bins.value), np.zeros(bins.value), np.zeros(bins.value)
+total_data = ColumnDataSource(data=dict(x=zz, y0=psi_dat, y1=cat_dat, y2=an_dat))
+
+# set up figures
+tools = "pan,wheel_zoom,box_zoom,reset,save,box_select,lasso_select"
+potential = figure(plot_height=500, plot_width=500, title="electrostatic potential", tools=tools,
+                   x_axis_label='z-distance [nm]', y_axis_label='\u03C6 [mV]')
+dens = figure(plot_height=500, plot_width=500, title="ion concentration", x_range=potential.x_range,  # linking plots together in x-axis
+              x_axis_label='z-distance [nm]', y_axis_label='c [mol/l]', tools=tools)
+# create plots
+psi_plt = potential.circle('x', 'y0', source=total_data, color='black')
+cat_plt = dens.circle('x', 'y1', source=total_data, color='red', legend='cation concentration')
+ani_plt = dens.circle('x', 'y2', source=total_data, color='blue', legend='anion concentration')
+
 
 # Set up layouts and add to document
 inputs = widgetbox(bins, temp, dist, valency, sigma, c_0)
-curdoc().add_root(row(inputs, potential, dens, width=1500))
+all_plots = gridplot([[inputs, potential, dens]])  # make plots linked
+curdoc().add_root(all_plots)
 curdoc().title = "pbe-visualization"
 ##########################################################################
