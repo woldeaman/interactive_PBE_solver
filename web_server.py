568c86a6863e4b77b87abfb020f51c26b15e8d6b
woldeaman <amanuel.woldekidan@gmail.com>
2019-02-16 21:11:17 +0100

    [WIP] adding free draw input for charge epsilon and pmfs

--- web_server.py
+++ web_server.py
@@ -3,9 +3,11 @@ import numpy as np
 import pbe_solver.pbe_solver as pbe  # this needs to be installed beforehand
 import scipy.constants as sc
 from bokeh.io import curdoc
-from bokeh.layouts import widgetbox, gridplot
+from bokeh.models.layouts import WidgetBox, Row, Column
+from bokeh.layouts import layout
 from bokeh.models import ColumnDataSource
 from bokeh.models.widgets import Slider
+from bokeh.models import FreehandDrawTool
 from bokeh.plotting import figure
 
 ##################
@@ -60,6 +62,13 @@ def update_data(attrname, old, new):
     # rewrite data
     total_data.data = dict(x=pbe_data['zz']-pbe_data['zz'].max()/2, y0=pbe_data['psi'],
                            y1=pbe_data['dens_p']*nm_3_to_mols, y2=pbe_data['dens_n']*nm_3_to_mols)
+    for store in [chrg_dat, eps_dat]:  # update zz-vector of free draw data
+        current_dat = store.data
+        store.data = dict(xs=[pbe_data['zz']-pbe_data['zz'].max()/2],
+                          ys=current_dat['ys'])
+    current_pmf = pmf_dat.data
+    pmf_dat.data = dict(xs=[pbe_data['zz']-pbe_data['zz'].max()/2]*2,
+                        ys=current_pmf['ys'])
 ##########################################################################
 
 
@@ -81,22 +90,44 @@ for w in [bins, temp, dist, valency, sigma, c_0]:
 zz = np.linspace(-dist.value/2, dist.value/2, bins.value)
 psi_dat, cat_dat, an_dat = np.zeros(bins.value), np.zeros(bins.value), np.zeros(bins.value)
 total_data = ColumnDataSource(data=dict(x=zz, y0=psi_dat, y1=cat_dat, y2=an_dat))
+# plot charge, epsilon and pmf
+chrg, eps, pmf_cat, pmf_an = np.zeros(bins.value), np.ones(bins.value)*80, np.zeros(bins.value), np.zeros(bins.value)
+chrg_dat, eps_dat = ColumnDataSource(data=dict(xs=[zz], ys=[chrg])), ColumnDataSource(data=dict(xs=[zz], ys=[eps]))
+pmf_dat = ColumnDataSource(data=dict(xs=[zz, zz], ys=[pmf_cat, pmf_an]))
 
 # set up figures
 tools = "pan,wheel_zoom,box_zoom,reset,save,box_select,lasso_select"
-potential = figure(plot_height=500, plot_width=500, title="electrostatic potential", tools=tools,
-                   x_axis_label='z-distance [nm]', y_axis_label='\u03C6 [mV]')
-dens = figure(plot_height=500, plot_width=500, title="ion concentration", x_range=potential.x_range,  # linking plots together in x-axis
-              x_axis_label='z-distance [nm]', y_axis_label='c [mol/l]', tools=tools)
-# create plots
-psi_plt = potential.circle('x', 'y0', source=total_data, color='black')
-cat_plt = dens.circle('x', 'y1', source=total_data, color='red', legend='cation concentration')
-ani_plt = dens.circle('x', 'y2', source=total_data, color='blue', legend='anion concentration')
+potential = figure(plot_height=700, plot_width=500, title="electrostatic potential", tools=tools,
+                   x_axis_label='z-distance [nm]', y_axis_label='\u03C6(z) [mV]')
+dens = figure(plot_height=700, plot_width=500, title="ion concentration", x_range=potential.x_range,  # linking plots together in x-axis
+              x_axis_label='z-distance [nm]', y_axis_label='c(z) [mol/l]', tools=tools)
+# create drawing plots for charge, epsilon and pmfs
+charge = figure(plot_width=300, plot_height=150, x_range=potential.x_range,
+                y_axis_label='\u03C1(z) [e/m\u00B3]', tools='reset,save', y_range=(-2, 2))
+epsilon = figure(plot_width=300, plot_height=150, x_range=potential.x_range, y_axis_label='\u03B5(z)',
+                 tools='reset,save', y_range=(0, 100))
+pmfs = figure(plot_width=300, plot_height=150, x_range=potential.x_range, x_axis_label='z-distance [nm]',
+              y_axis_label='PMF(z) [k\u0299T]', tools='reset,save', y_range=(-10, 10))
 
+# create plots
+psi_plt = potential.line('x', 'y0', source=total_data, color='black', line_width=3)
+cat_plt = dens.line('x', 'y1', source=total_data, color='red', legend='cation concentration', line_width=3)
+ani_plt = dens.line('x', 'y2', source=total_data, color='blue', legend='anion concentration', line_width=3)
+# set up figures with drawing tools
+for fig, dat in zip([charge, epsilon], [chrg_dat, eps_dat]):
+    plt = fig.multi_line(xs='xs', ys='ys', source=dat, color='black', line_width=3)
+    draw_tool = FreehandDrawTool(renderers=[plt], num_objects=1)
+    fig.add_tools(draw_tool)
+pmf_plt = pmfs.multi_line(xs='xs', ys='ys', source=pmf_dat, color='red', line_width=3)
+draw_tool_p = FreehandDrawTool(renderers=[pmf_plt], num_objects=2)
+pmfs.add_tools(draw_tool_p)
+pmfs.toolbar.active_drag = draw_tool_p
 
 # Set up layouts and add to document
-inputs = widgetbox(bins, temp, dist, valency, sigma, c_0)
-all_plots = gridplot([[inputs, potential, dens]])  # make plots linked
+inputs = WidgetBox(bins, temp, dist, valency, sigma, c_0)
+draw_inputs = Column(inputs, charge, epsilon, pmfs)
+pot_dens_plt = Row(potential, dens)  # make plots linked
+all_plots = layout([[draw_inputs, pot_dens_plt]])
 curdoc().add_root(all_plots)
 curdoc().title = "pbe-visualization"
 ##########################################################################
