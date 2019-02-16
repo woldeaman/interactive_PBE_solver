79b04fd571541aabf101ec56dfdc2a3531614d8e
woldeaman <amanuel.woldekidan@gmail.com>
2019-02-16 23:15:40 +0100

    now works with drawing pmfs! some fine tuning still possible though

--- web_server.py
+++ web_server.py
@@ -2,11 +2,12 @@
 import numpy as np
 import pbe_solver.pbe_solver as pbe  # this needs to be installed beforehand
 import scipy.constants as sc
+import scipy.interpolate as ip
 from bokeh.io import curdoc
 from bokeh.models.layouts import WidgetBox, Row, Column
 from bokeh.layouts import layout
 from bokeh.models import ColumnDataSource
-from bokeh.models.widgets import Slider
+from bokeh.models.widgets import Slider, Button
 from bokeh.models import FreehandDrawTool
 from bokeh.plotting import figure
 
@@ -20,11 +21,8 @@ nm_3_to_mols = 1E24/sc.Avogadro  # from nm^3 to mol/l
 ###########################
 #       FUNCTIONS        #
 ##########################################################################
-def solver(bins, temp, distance, valency, sigma, c_0_pre):
+def solver(bins, temp, distance, valency, sigma, c_0_pre, rho, eps, pmf_cat, pmf_an):
     """Run the PBE solver."""
-    # TODO: implement reading of PMFs, epsilon and charge density profiles
-    rho, eps, pmf_cat, pmf_an = np.zeros(bins), np.ones(bins)*(1/80), np.zeros(bins), np.zeros(bins)
-
     # convert units
     (zz_hat, kappa, c_0, beta, dz_hat, sigma_hat, rho_hat) = pbe.convert_units(
         bins, temp, distance, valency, sigma, rho, c_0_pre)
@@ -46,8 +44,30 @@ def solver(bins, temp, distance, valency, sigma, c_0_pre):
     return data
 
 
-def update_data(attrname, old, new):
+def interpolate_free_draw_data(poly_order=1):
+    """Do interpolation to update free draw data and profiles."""
+    zz = np.linspace(-dist.value/2, 0, bins.value)
+    for store in [chrg_dat, eps_dat]:
+        xx, yy = store.data['xs'][0], store.data['ys'][0]  # interpolate data
+        xx_unq, idx_unq = np.unique(xx, return_index=True)
+        spl = ip.UnivariateSpline(xx_unq[xx_unq.argsort()],
+                                  np.array(yy)[idx_unq][xx_unq.argsort()],
+                                  s=0, k=poly_order, ext='const')
+        store.data = dict(xs=[zz], ys=[spl(zz)])
+
+    pmf_spls = []
+    for i in range(2):
+        xx, yy = pmf_dat.data['xs'][i], pmf_dat.data['ys'][i]  # interpolate pmf data
+        xx_unq, idx_unq = np.unique(xx, return_index=True)
+        pmf_spls.append(ip.UnivariateSpline(xx_unq[xx_unq.argsort()],
+                                            np.array(yy)[idx_unq][xx_unq.argsort()],
+                                            s=0, k=poly_order, ext='const'))
+    pmf_dat.data = dict(xs=[zz]*2, ys=[pmf_spls[i](zz) for i in range(2)])
+
+
+def update_data():
     """Callback function to update parameters."""
+    interpolate_free_draw_data()  # first interpolate free draw data
     # Get the current slider values
     b = bins.value
     t = temp.value
@@ -55,20 +75,17 @@ def update_data(attrname, old, new):
     z = valency.value
     s = sigma.value
     c = c_0.value
+    r = chrg_dat.data['ys'][0]
+    e = 1/eps_dat.data['ys'][0]  # inverse epsilon is input
+    p_c = pmf_dat.data['ys'][0]
+    p_a = pmf_dat.data['ys'][1]
 
     # solve pbe
-    pbe_data = solver(b, t, d, z, s, c)
+    pbe_data = solver(b, t, d, z, s, c, r, e, p_c, p_a)
 
     # rewrite data
     total_data.data = dict(x=pbe_data['zz']-pbe_data['zz'].max()/2, y0=pbe_data['psi'],
                            y1=pbe_data['dens_p']*nm_3_to_mols, y2=pbe_data['dens_n']*nm_3_to_mols)
-    for store in [chrg_dat, eps_dat]:  # update zz-vector of free draw data
-        current_dat = store.data
-        store.data = dict(xs=[pbe_data['zz']-pbe_data['zz'].max()/2],
-                          ys=current_dat['ys'])
-    current_pmf = pmf_dat.data
-    pmf_dat.data = dict(xs=[pbe_data['zz']-pbe_data['zz'].max()/2]*2,
-                        ys=current_pmf['ys'])
 ##########################################################################
 
 
@@ -81,19 +98,19 @@ temp = Slider(title="temperatur [K]", value=300, start=0, end=500, step=10)
 dist = Slider(title="distance [nm]", value=1.0, start=0.1, end=10, step=0.1)
 valency = Slider(title="valency", value=1, start=1, end=4, step=1)
 sigma = Slider(title="surface charge [e/nm\u00b2]", value=0, start=-5, end=5, step=0.1)
-c_0 = Slider(title="ion concentration [mol/l]", value=0, start=1, end=5, step=0.1)
-# connect same callback function to all sliders
-for w in [bins, temp, dist, valency, sigma, c_0]:
-    w.on_change('value', update_data)
+c_0 = Slider(title="ion concentration [mol/l]", value=1, start=1, end=5, step=0.1)
+start = Button(label="solve", button_type="primary")
+start.on_click(update_data)  # update plots when clicking button
 
 # set up and initiallize data
 zz = np.linspace(-dist.value/2, dist.value/2, bins.value)
 psi_dat, cat_dat, an_dat = np.zeros(bins.value), np.zeros(bins.value), np.zeros(bins.value)
 total_data = ColumnDataSource(data=dict(x=zz, y0=psi_dat, y1=cat_dat, y2=an_dat))
 # plot charge, epsilon and pmf
+zz_half = np.linspace(-dist.value/2, 0, bins.value)
 chrg, eps, pmf_cat, pmf_an = np.zeros(bins.value), np.ones(bins.value)*80, np.zeros(bins.value), np.zeros(bins.value)
-chrg_dat, eps_dat = ColumnDataSource(data=dict(xs=[zz], ys=[chrg])), ColumnDataSource(data=dict(xs=[zz], ys=[eps]))
-pmf_dat = ColumnDataSource(data=dict(xs=[zz, zz], ys=[pmf_cat, pmf_an]))
+chrg_dat, eps_dat = ColumnDataSource(data=dict(xs=[zz_half], ys=[chrg])), ColumnDataSource(data=dict(xs=[zz_half], ys=[eps]))
+pmf_dat = ColumnDataSource(data=dict(xs=[zz_half, zz_half], ys=[pmf_cat, pmf_an]))
 
 # set up figures
 tools = "pan,wheel_zoom,box_zoom,reset,save,box_select,lasso_select"
@@ -102,11 +119,11 @@ potential = figure(plot_height=700, plot_width=500, title="electrostatic potenti
 dens = figure(plot_height=700, plot_width=500, title="ion concentration", x_range=potential.x_range,  # linking plots together in x-axis
               x_axis_label='z-distance [nm]', y_axis_label='c(z) [mol/l]', tools=tools)
 # create drawing plots for charge, epsilon and pmfs
-charge = figure(plot_width=300, plot_height=150, x_range=potential.x_range,
-                y_axis_label='\u03C1(z) [e/m\u00B3]', tools='reset,save', y_range=(-2, 2))
-epsilon = figure(plot_width=300, plot_height=150, x_range=potential.x_range, y_axis_label='\u03B5(z)',
+charge = figure(plot_width=300, plot_height=150, y_axis_label='\u03C1(z) [e/m\u00B3]',
+                tools='reset,save', y_range=(-2, 2))
+epsilon = figure(plot_width=300, plot_height=150, x_range=charge.x_range, y_axis_label='\u03B5(z)',
                  tools='reset,save', y_range=(0, 100))
-pmfs = figure(plot_width=300, plot_height=150, x_range=potential.x_range, x_axis_label='z-distance [nm]',
+pmfs = figure(plot_width=300, plot_height=150, x_range=charge.x_range, x_axis_label='z-distance [nm]',
               y_axis_label='PMF(z) [k\u0299T]', tools='reset,save', y_range=(-10, 10))
 
 # create plots
@@ -124,7 +141,7 @@ pmfs.add_tools(draw_tool_p)
 pmfs.toolbar.active_drag = draw_tool_p
 
 # Set up layouts and add to document
-inputs = WidgetBox(bins, temp, dist, valency, sigma, c_0)
+inputs = WidgetBox(start, bins, temp, dist, valency, sigma, c_0)
 draw_inputs = Column(inputs, charge, epsilon, pmfs)
 pot_dens_plt = Row(potential, dens)  # make plots linked
 all_plots = layout([[draw_inputs, pot_dens_plt]])
