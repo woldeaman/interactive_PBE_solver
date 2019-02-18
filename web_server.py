9227abda567c26846bf197f672ebf6204f7300d9
Amanuel Wolde-Kidan <woldeaman@hiraiwamac.physik.fu-berlin.de>
2019-02-18 12:21:19 +0100

    tydied up script a bit

--- web_server.py
+++ web_server.py
@@ -21,6 +21,74 @@ nm_3_to_mols = 1E24/sc.Avogadro  # from nm^3 to mol/l
 ###########################
 #       FUNCTIONS        #
 ##########################################################################
+def initialize_widgets():
+    """Set up all the widgets."""
+    global bins  # make them global to be accessable throughout the script
+    bins = Slider(title="Bins", value=100, start=50, end=250, step=10)
+    global temp
+    temp = Slider(title="Temperatur [K]", value=300, start=0, end=500, step=10)
+    global dist
+    dist = Slider(title="Distance [nm]", value=5, start=0.1, end=10, step=0.1)
+    global valency
+    valency = Slider(title="Ion Valency", value=1, start=1, end=4, step=1)
+    global sigma
+    sigma = Slider(title="Surface Charge [e/nm\u00b2]", value=0, start=-5, end=5, step=0.1)
+    global c_0
+    c_0 = Slider(title="Ion Concentration [mol/l]", value=1, start=1, end=5, step=0.1)
+    global start
+    start = Button(label="SOLVE", button_type="primary")
+    start.on_click(update_data)  # update plots when clicking button
+
+
+def initialize_data():
+    """Set up and initiallize all data stores."""
+    zz = np.linspace(-dist.value/2, dist.value/2, bins.value)
+    psi_dat, cat_dat, an_dat = np.zeros(bins.value), np.zeros(bins.value), np.zeros(bins.value)
+    global total_data
+    total_data = ColumnDataSource(data=dict(x=zz, y0=psi_dat, y1=cat_dat, y2=an_dat))
+    # plot charge, epsilon and pmf
+    zz_half = np.linspace(-dist.value/2, 0, bins.value)
+    chrg, eps, pmf_cat, pmf_an = np.zeros(bins.value), np.ones(bins.value)*80, np.zeros(bins.value), np.zeros(bins.value)
+    global chrg_dat, eps_dat, pmf_dat
+    chrg_dat, eps_dat = ColumnDataSource(data=dict(xs=[zz_half], ys=[chrg])), ColumnDataSource(data=dict(xs=[zz_half], ys=[eps]))
+    pmf_dat = ColumnDataSource(data=dict(xs=[zz_half, zz_half], ys=[pmf_cat, pmf_an]))
+
+
+def build_figures():
+    """Create figures."""
+    tools = "pan,wheel_zoom,box_zoom,reset,save,box_select,lasso_select"
+    global potential
+    potential = figure(plot_height=800, plot_width=800, title="electrostatic potential", tools=tools,
+                       x_axis_label='z-distance [nm]', y_axis_label='\u03C6(z) [mV]')
+    global dens
+    dens = figure(plot_height=800, plot_width=800, title="ion concentration", x_range=potential.x_range,  # linking plots together in x-axis
+                  x_axis_label='z-distance [nm]', y_axis_label='c(z) [mol/l]', tools=tools)
+    # create drawing plots for charge, epsilon and pmfs
+    global charge
+    charge = figure(plot_width=300, plot_height=155, y_axis_label='\u03C1(z) [e/m\u00B3]',
+                    tools='pan,ywheel_zoom,save', y_range=(-2, 2))
+    global epsilon
+    epsilon = figure(plot_width=300, plot_height=155, x_range=charge.x_range, y_axis_label='\u03B5(z)',
+                     tools='pan,ywheel_zoom,save', y_range=(0, 100))
+    global pmfs
+    pmfs = figure(plot_width=300, plot_height=185, x_range=charge.x_range, x_axis_label='z-distance [nm]',
+                  y_axis_label='PMF(z) [k\u0299T]', tools='pan,ywheel_zoom,save', y_range=(-10, 10))
+
+    # create plots
+    potential.line('x', 'y0', source=total_data, color='black', line_width=3)
+    dens.line('x', 'y1', source=total_data, color='red', legend='Cation Concentration', line_width=3)
+    dens.line('x', 'y2', source=total_data, color='blue', legend='Anion Concentration', line_width=3)
+    # set up figures with drawing tools
+    for fig, dat in zip([charge, epsilon], [chrg_dat, eps_dat]):
+        plt = fig.multi_line(xs='xs', ys='ys', source=dat, color='black', line_width=3)
+        draw_tool = FreehandDrawTool(renderers=[plt], num_objects=1)
+        fig.add_tools(draw_tool)
+    pmf_plt = pmfs.multi_line(xs='xs', ys='ys', source=pmf_dat, line_width=3)
+    draw_tool_p = FreehandDrawTool(renderers=[pmf_plt], num_objects=2)
+    pmfs.add_tools(draw_tool_p)
+    pmfs.toolbar.active_drag = draw_tool_p
+
+
 def solver(bins, temp, distance, valency, sigma, c_0_pre, rho, eps, pmf_cat, pmf_an):
     """Run the PBE solver."""
     # convert units
@@ -44,7 +112,7 @@ def solver(bins, temp, distance, valency, sigma, c_0_pre, rho, eps, pmf_cat, pmf
     return data
 
 
-def interpolate_free_draw_data(poly_order=1):
+def interpolate_free_draw_data():
     """Do interpolation to update free draw data and profiles."""
     zz = np.linspace(-dist.value/2, 0, bins.value)
     for store in [chrg_dat, eps_dat]:
@@ -52,16 +120,17 @@ def interpolate_free_draw_data(poly_order=1):
         xx_unq, idx_unq = np.unique(xx, return_index=True)
         spl = ip.UnivariateSpline(xx_unq[xx_unq.argsort()],
                                   np.array(yy)[idx_unq][xx_unq.argsort()],
-                                  s=0, k=poly_order, ext='const')
+                                  s=0, k=1, ext='const')
         store.data = dict(xs=[zz], ys=[spl(zz)])
 
     pmf_spls = []
+    current_pmf_dat = pmf_dat.data
     for i in range(2):
-        xx, yy = pmf_dat.data['xs'][i], pmf_dat.data['ys'][i]  # interpolate pmf data
+        xx, yy = current_pmf_dat['xs'][i], current_pmf_dat['ys'][i]  # interpolate pmf data
         xx_unq, idx_unq = np.unique(xx, return_index=True)
         pmf_spls.append(ip.UnivariateSpline(xx_unq[xx_unq.argsort()],
                                             np.array(yy)[idx_unq][xx_unq.argsort()],
-                                            s=0, k=poly_order, ext='const'))
+                                            s=0, k=1, ext='const'))
     pmf_dat.data = dict(xs=[zz]*2, ys=[pmf_spls[i](zz) for i in range(2)])
 
 
@@ -93,52 +162,11 @@ def update_data():
 #      MAIN        #
 ##########################################################################
 # set up and initiallize widgets
-bins = Slider(title="bins", value=100, start=50, end=250, step=10)
-temp = Slider(title="temperatur [K]", value=300, start=0, end=500, step=10)
-dist = Slider(title="distance [nm]", value=5, start=0.1, end=10, step=0.1)
-valency = Slider(title="valency", value=1, start=1, end=4, step=1)
-sigma = Slider(title="surface charge [e/nm\u00b2]", value=0, start=-5, end=5, step=0.1)
-c_0 = Slider(title="ion concentration [mol/l]", value=1, start=1, end=5, step=0.1)
-start = Button(label="solve", button_type="primary")
-start.on_click(update_data)  # update plots when clicking button
-
+initialize_widgets()
 # set up and initiallize data
-zz = np.linspace(-dist.value/2, dist.value/2, bins.value)
-psi_dat, cat_dat, an_dat = np.zeros(bins.value), np.zeros(bins.value), np.zeros(bins.value)
-total_data = ColumnDataSource(data=dict(x=zz, y0=psi_dat, y1=cat_dat, y2=an_dat))
-# plot charge, epsilon and pmf
-zz_half = np.linspace(-dist.value/2, 0, bins.value)
-chrg, eps, pmf_cat, pmf_an = np.zeros(bins.value), np.ones(bins.value)*80, np.zeros(bins.value), np.zeros(bins.value)
-chrg_dat, eps_dat = ColumnDataSource(data=dict(xs=[zz_half], ys=[chrg])), ColumnDataSource(data=dict(xs=[zz_half], ys=[eps]))
-pmf_dat = ColumnDataSource(data=dict(xs=[zz_half, zz_half], ys=[pmf_cat, pmf_an]))
-
+initialize_data()
 # set up figures
-tools = "pan,wheel_zoom,box_zoom,reset,save,box_select,lasso_select"
-potential = figure(plot_height=700, plot_width=500, title="electrostatic potential", tools=tools,
-                   x_axis_label='z-distance [nm]', y_axis_label='\u03C6(z) [mV]')
-dens = figure(plot_height=700, plot_width=500, title="ion concentration", x_range=potential.x_range,  # linking plots together in x-axis
-              x_axis_label='z-distance [nm]', y_axis_label='c(z) [mol/l]', tools=tools)
-# create drawing plots for charge, epsilon and pmfs
-charge = figure(plot_width=300, plot_height=150, y_axis_label='\u03C1(z) [e/m\u00B3]',
-                tools='reset,save', y_range=(-2, 2))
-epsilon = figure(plot_width=300, plot_height=150, x_range=charge.x_range, y_axis_label='\u03B5(z)',
-                 tools='reset,save', y_range=(0, 100))
-pmfs = figure(plot_width=300, plot_height=150, x_range=charge.x_range, x_axis_label='z-distance [nm]',
-              y_axis_label='PMF(z) [k\u0299T]', tools='reset,save', y_range=(-10, 10))
-
-# create plots
-psi_plt = potential.line('x', 'y0', source=total_data, color='black', line_width=3)
-cat_plt = dens.line('x', 'y1', source=total_data, color='red', legend='cation concentration', line_width=3)
-ani_plt = dens.line('x', 'y2', source=total_data, color='blue', legend='anion concentration', line_width=3)
-# set up figures with drawing tools
-for fig, dat in zip([charge, epsilon], [chrg_dat, eps_dat]):
-    plt = fig.multi_line(xs='xs', ys='ys', source=dat, color='black', line_width=3)
-    draw_tool = FreehandDrawTool(renderers=[plt], num_objects=1)
-    fig.add_tools(draw_tool)
-pmf_plt = pmfs.multi_line(xs='xs', ys='ys', source=pmf_dat, color='red', line_width=3)
-draw_tool_p = FreehandDrawTool(renderers=[pmf_plt], num_objects=2)
-pmfs.add_tools(draw_tool_p)
-pmfs.toolbar.active_drag = draw_tool_p
+build_figures()
 
 # Set up layouts and add to document
 inputs = WidgetBox(start, bins, temp, dist, valency, sigma, c_0)
